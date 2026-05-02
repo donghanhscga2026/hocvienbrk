790fc045cf2db990a339567d187987e6f76045c1
Coach Cương <donghanh.scga2026@gmail.com>
2026-05-02 08:09:05 +0700

    Xóa các file debug, check, và temp thừa

--- check_sys.ts
+++
@@ -1,7 +0,0 @@
-import prisma from './lib/prisma'
-
-async function check() {
-    const sys = await prisma.system.findMany({ take: 5 })
-    console.log(sys)
-}
-check().finally(() => prisma.$disconnect())
